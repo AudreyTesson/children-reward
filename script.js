dee6668762bf24f4fa54aeacc90b112af3634605
Audrey Tesson <audrey.tesson.pro@outlook.fr>
2023-03-03 16:38:46 +0100

    let->const, reste à changer le onclick avec un écouteur

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 function piocherCarte() {
-    let paquet = [
-      "ACTIVITE", 
-      "ARGENT", 
-      "CUISINER", 
-      "DESSIN", 
-      "GYM", 
-      "LIVRE EN PLUS", 
-      "ORDINATEUR", 
-      "REPAS TV", 
-      "REPAS SPECIAL", 
-      "SUCRERIES", 
-      "TELE", 
-      "TELEPHONE", 
-      "TRESOR", 
-      "VELO", 
-      "YOUTUBE"
-    ];
-  
-let carte = paquet[Math.floor(Math.random() * paquet.length)];
-let cheminImage = "images/" + carte + ".png";
+  const paquet = [
+    "ACTIVITE",
+    "ARGENT",
+    "CUISINER",
+    "DESSIN",
+    "GYM",
+    "LIVRE EN PLUS",
+    "ORDINATEUR",
+    "REPAS TV",
+    "REPAS SPECIAL",
+    "SUCRERIES",
+    "TELE",
+    "TELEPHONE",
+    "TRESOR",
+    "VELO",
+    "YOUTUBE",
+  ];
 
-document.getElementById("carte-nom").innerHTML = carte;
-document.getElementById("carte-image").src = cheminImage;
+  let carte = paquet[Math.floor(Math.random() * paquet.length)];
+  let cheminImage = "images/" + carte + ".png";
+
+  document.getElementById("carte-nom").innerHTML = carte;
+  document.getElementById("carte-image").src = cheminImage;
 }
-  
