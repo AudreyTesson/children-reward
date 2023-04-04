fdee777fbeab7b74827db62bafad746c0378fc34
Audrey Tesson <audrey.tesson.pro@outlook.fr>
2023-04-04 16:57:39 +0200

    changes due to picture names

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function piocherCarte() {
   const paquet = [
-    "activité",
-    "argent",
-    "cuisiner",
-    "dessin",
-    "gym",
-    "livre en plus",
-    "ordinateur",
-    "repas télé",
-    "repas spécial",
-    "sucreries",
-    "télé",
-    "téléphone",
-    "trésor",
-    "vélo",
-    "youtube",
+    {nom: "Activité physique", image: "activite"},
+    {nom: "Argent de poche", image: "argent"},
+    {nom: "Cuisiner un repas", image: "cuisiner"},
+    {nom: "Dessiner ou peindre", image: "dessin"},
+    {nom: "Faire du sport", image: "gym"},
+    {nom: "Lire un livre", image: "livre en plus"},
+    {nom: "Utiliser l'ordinateur", image: "ordinateur"},
+    {nom: "Manger devant la télé", image: "repas tele"},
+    {nom: "Manger un repas spécial", image: "repas special"},
+    {nom: "Manger des sucreries", image: "sucreries"},
+    {nom: "Regarder la télé", image: "tele"},
+    {nom: "Utiliser le téléphone", image: "telephone"},
+    {nom: "Trouver un trésor", image: "tresor"},
+    {nom: "Faire du vélo", image: "velo"},
+    {nom: "Regarder des vidéos sur Youtube", image: "youtube"}
   ];
 
   let carte = paquet[Math.floor(Math.random() * paquet.length)];
-  let cheminImage = "images/" + carte + ".png";
+  let cheminImage = "images/" + carte.image + ".png";
 
-  document.getElementById("carte-nom-majuscules").innerHTML = carte.toUpperCase();
-  document.getElementById("carte-nom-minuscules").innerHTML = carte.toLowerCase();
+  document.getElementById("carte-nom-majuscules").innerHTML = carte.nom.toUpperCase();
+  document.getElementById("carte-nom-minuscules").innerHTML = carte.nom.toLowerCase();
   document.getElementById("carte-image").src = cheminImage;
 }
